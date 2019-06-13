e24b010d002afe722a348233c0a4a02fb367a1b7
pc-10 <pc-10@pc-10-PC>
2019-06-13 18:35:56 +0900

    fuck

---
+++ StudyCookie/src/com/session.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/session")
+public class session extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("id", "sk");
+		String id = (String)session.getAttribute("id");
+		
+		session.removeAttribute("id");
+	}
+
+}
